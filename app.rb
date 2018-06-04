8f3a32536ba382736cd662095d40e83c2af02aea
Eric Razor, Jr <eric.razorjr@gmail.com>
2018-06-04 16:51:20 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
 	get '/' do
-		
+		erb :index
+	end
+
+	# get "/index" do
+	# 	"testing home page"
+	# end
+
+	get "/info" do
+		erb :info
 	end
 end
